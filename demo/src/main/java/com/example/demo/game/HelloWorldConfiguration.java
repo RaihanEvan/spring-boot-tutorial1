376a347e3673ffca992b522b3110845932b4a148
Raihan Evan <raihan.evan@gmail.com>
2023-03-30 01:11:55 +0600

    bean methods types and styles

--- demo/src/main/java/com/example/demo/game/HelloWorldConfiguration.java
+++ demo/src/main/java/com/example/demo/game/HelloWorldConfiguration.java
@@ -3,7 +3,7 @@ package com.example.demo.game;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person(String name, int age){ };
+record Person(String name, int age, Address address){ };
 record Address(String firstLine, String city){};
 @Configuration
 public class HelloWorldConfiguration {
@@ -17,10 +17,20 @@ public class HelloWorldConfiguration {
     }
     @Bean
     public Person person(){
-        var person = new Person("Ethar",11);
+        var person = new Person("Ethar",11, new Address(
+                "Debidwar","Cumilla"
+        ));
         return person;
     }
     @Bean
+    public Person person2MethodCall(){
+        return new Person(name(),age(),address());
+    }
+    @Bean
+    public Person person3Parameters(String name,int age, Address address2){
+        return new Person(name,age,address2);
+    }
+    @Bean(name = "address2")
     public Address address(){
         return new Address("Setabganj","Dinajpur");
     }
