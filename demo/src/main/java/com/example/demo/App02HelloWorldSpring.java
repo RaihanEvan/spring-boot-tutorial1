aed96510aea2dfd1a9b1ca583e86017a883b554d
Raihan Evan <raihan.evan@gmail.com>
2023-03-29 20:08:31 +0600

    first spring class created

--- demo/src/main/java/com/example/demo/App02HelloWorldSpring.java
+++ demo/src/main/java/com/example/demo/App02HelloWorldSpring.java
@@ -2,11 +2,9 @@ package com.example.demo;
 
 
 import com.example.demo.game.GameRunner;
-import com.example.demo.game.MarioGame;
 import com.example.demo.game.PacmanGame;
-import com.example.demo.game.SuperContraGame;
 
-public class AppGamingBasicClass {
+public class App02HelloWorldSpring {
 
 	public static void main(String[] args) {
 		//var game = new SuperContraGame();
