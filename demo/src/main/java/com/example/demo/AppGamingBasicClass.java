f204973e952cadefd82c6a723e3cf900246096f4
Raihan Evan <raihan.evan@gmail.com>
2023-03-29 19:44:08 +0600

    implemented pacman game

--- demo/src/main/java/com/example/demo/AppGamingBasicClass.java
+++ demo/src/main/java/com/example/demo/AppGamingBasicClass.java
@@ -3,13 +3,15 @@ package com.example.demo;
 
 import com.example.demo.game.GameRunner;
 import com.example.demo.game.MarioGame;
+import com.example.demo.game.PacmanGame;
 import com.example.demo.game.SuperContraGame;
 
 public class AppGamingBasicClass {
 
 	public static void main(String[] args) {
 		//var game = new SuperContraGame();
-		var game = new MarioGame();
+		//var game = new MarioGame();
+		var game = new PacmanGame();
 
 		var gameRunner = new GameRunner(game);
 		gameRunner.run();
